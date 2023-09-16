7d21152accb5a57305479271f54586eec909ec04
zanozano <zanozanozano@gmail.com>
2023-09-16 23:36:03 +0700

    update equipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,6 @@
 class Equipment < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
     has_many :maintainers, dependent: :destroy
     mount_uploader :photo, PhotoUploader
     has_one_attached :photo
